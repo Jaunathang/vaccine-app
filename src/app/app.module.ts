b99d4309aa7e059d6eece32240af266080a229f3
Jaune <jaunathang@gmail.com>
2021-04-18 00:00:25 -0400

    Test avec le theme custom et ajout de CustomMaterialModule permettant d'accéder à tous les components de Material partout dans l'app. Je ferai le ménage de ce qui n'est pas utilisé avant de déployer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//----- Modules -----//
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CustomMaterialModule } from  './custom-material/custom-material.module'
+
+//----- Components -----//
 import { AccueilComponent } from './accueil/accueil.component';
 import { MaladieComponent } from './maladie/maladie.component';
 import { VaccinComponent } from './vaccin/vaccin.component';
 import { PreventionComponent } from './prevention/prevention.component';
 import { TestComponent } from './test/test.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +31,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     AppRoutingModule,
     BrowserAnimationsModule,
     FontAwesomeModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
